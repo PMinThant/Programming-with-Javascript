df9e1edf89b63954b980c14399d12e368dd43405
Phyoe Min Thant <pminthant@outlook.com>
2023-08-10 17:00:20 +0700

    Week2

---
+++ creatingArraysAndObjects.js
@@ -0,0 +1,10 @@
+var clothes = [];
+clothes.push("shirt", "trousers", "pants", "suits", "vests");
+clothes.pop();
+clothes.push("underwears")
+console.log(clothes[2]);
+
+var favCar = {};
+favCar.color = "red";
+favCar.convertable = true;
+console.log(favCar)
---
+++ errortest.js
@@ -0,0 +1,2 @@
+console.log(username);
+var a "there's no assignment operator here";
